7b17506bbb0f40383be2470b42488d8f974a8e1e
MattR246 <49105348+MattR246@users.noreply.github.com>
2019-09-04 13:23:32 -0500

    Updates

---
+++ precache-manifest.97024a1adcb5b1bd3fb7bdfbfe801778.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "216cbf989272b661f31f85b837169d4c",
+    "url": "/robofoes/index.html"
+  },
+  {
+    "revision": "b85901d18715e564b892",
+    "url": "/robofoes/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "6388249d3d77d90e314d",
+    "url": "/robofoes/static/css/main.fb505a8d.chunk.css"
+  },
+  {
+    "revision": "b85901d18715e564b892",
+    "url": "/robofoes/static/js/2.6bd93c0c.chunk.js"
+  },
+  {
+    "revision": "6388249d3d77d90e314d",
+    "url": "/robofoes/static/js/main.f5c54f17.chunk.js"
+  },
+  {
+    "revision": "d6488fd111aef27aa62f",
+    "url": "/robofoes/static/js/runtime~main.2fc2d11b.js"
+  },
+  {
+    "revision": "a57a3c0c102c29c947c918ef8e6b92bf",
+    "url": "/robofoes/static/media/megaman_2.a57a3c0c.woff"
+  }
+]);
